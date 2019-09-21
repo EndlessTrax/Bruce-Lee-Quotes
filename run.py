25d7e288230a6c9a66688786781474d01bc60161
Ricky White <ricky@whitelionmedia.com>
2019-09-21 17:08:38 -0400

    added if __main__ and main() structure to script

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-for i in range(12):      # Number of pages plus one
-    url = "http://www.bruceleequotes.org/page/{}".format(i)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "lxml")
+def main():
+    for i in range(12):      # Number of pages plus one
+        url = "http://www.bruceleequotes.org/page/{}".format(i)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
 
-    # Second argument is for 'append'. Running the script twice or more in succession will duplicate the quotes. 
-    quotesFile = open('quotes.txt', 'a', encoding='utf-8')
+        # Second argument is for 'append'. Running the script twice or more in succession will duplicate the quotes. 
+        quotesFile = open('quotes.txt', 'a', encoding='utf-8')
 
-    for l in soup.find_all('h2', class_='title'):
-        quotesFile.write(l.text)
-        quotesFile.write('\n')
+        for l in soup.find_all('h2', class_='title'):
+            quotesFile.write(l.text)
+            quotesFile.write('\n')
+            print(l.text)
 
-    quotesFile.close()
+        quotesFile.close()
+
+if __name__ == "__main__":
+    main()
