4d85f5c31d542ab37f0e40eb9eb5a61ce1d4eb41
endlesstrax <ricky@whitelionmedia.com>
2020-06-11 08:42:24 -0400

    Light refactoring + painted black.

--- run.py
+++ run.py
@@ -1,21 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def main():
-    for i in range(12):      # Number of pages plus one
+    for i in range(12):  # Number of pages plus one
         url = "http://www.bruceleequotes.org/page/{}".format(i)
         r = requests.get(url)
         soup = BeautifulSoup(r.content, "html.parser")
 
-        # Second argument is for 'append'. Running the script twice or more in succession will duplicate the quotes. 
-        quotesFile = open('quotes.txt', 'a', encoding='utf-8')
+        with open("quotes.txt", "a", encoding="utf8") as quotesFile:
+            for l in soup.find_all("h2", class_="title"):
+                quotesFile.write(f"{l.text}\n")
+                print(f"Adding: {l.text}")
 
-        for l in soup.find_all('h2', class_='title'):
-            quotesFile.write(l.text)
-            quotesFile.write('\n')
-            print(l.text)
-
-        quotesFile.close()
 
 if __name__ == "__main__":
     main()
